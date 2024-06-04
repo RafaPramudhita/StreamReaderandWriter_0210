042204b25eaf943e05a15feb1ff41bff599e4f54
RafaPramudhita <161433424+RafaPramudhita@users.noreply.github.com>
2024-06-04 15:27:54 +0700

    membuat exception handling catch jika selain int maka akan di eksekusi

--- ManualException.cpp
+++ ManualException.cpp
@@ -11,5 +11,10 @@ int main(){
 		//blok ini akan dieksekusi
 		cout << " Pengecualian akan dieksekusi" << endl;
 	}
+	catch (...) {
+		/*Jika selain integer maka block ini akan dieksekusi*/
+		cout << "Default Pengecualian dieksekusi" << endl;
+	}
 	
+    
 }
