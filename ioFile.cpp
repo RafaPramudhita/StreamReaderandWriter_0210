e9e77777854e886387acb3d3af5efb39cdf36f14
RafaPramudhita <161433424+RafaPramudhita@users.noreply.github.com>
2024-06-04 14:53:38 +0700

    melakukan loop jika file ada, kalau ngga ada yaudah

--- ioFile.cpp
+++ ioFile.cpp
@@ -33,5 +33,19 @@ int main() {
 
 	cout << endl << ">= Membuka dan membaca file " << endl;
 
+	//jika file ada maka
+	if (infile.is_open())
+	{
+		// melakukan perulangan setiap baris
+		while (getline(infile, baris))
+		{
+			//dan tampilkan disini
+			cout << baris << '\n';
+		}
+		//tutup file tersebut setelah selesai
+		infile.close();
+	}
+	//jika tidak ditemukan file maka akan menampilkan ini
+	else cout << "unable to open file";
 	
 }
